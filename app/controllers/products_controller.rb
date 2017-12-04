faec9e8502e70e99fb5ce4110ecfe1fbc871fbe4
wafaa122 <wafa_7_@hotmail.com>
2017-12-04 13:00:32 +0200

    serach box

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,13 @@ class ProductsController < ApplicationController
 
   # GET /products
   # GET /products.json
-
   def index
-    @products = Product.all
+    if params[:q]
+      search_term = params[:q]
+      @products = Product.search(search_term)
+    else
+      @products = Product.all
+    end
   end
 
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   has_many :orders
+  def self.search(search_term)
+    like_type = Rails.env.production? ? "ilike" : "LIKE"
+    Product.where("name #{like_type} ?", "%#{search_term}%")
+  end
 end
