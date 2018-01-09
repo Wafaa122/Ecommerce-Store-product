f80fe633ea628914f435385c3d0c2081db2bf280
wafaa122 <wafa_7_@hotmail.com>
2018-01-09 13:55:30 +0200

    comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', controllers: { registrations: "user_registrations" } }
   resources :orders, only: [:index, :show, :create, :destroy]
   resources :products do
     resources :comments
   end
-  resources :users
+
   root 'simple_pages#index'
   get 'contact_form/new'
   get 'contact_form/create'
@@ -14,7 +13,7 @@ Rails.application.routes.draw do
   get 'simple_pages/landing_page'
   root 'simple_pages#index'
   post 'simple_pages/thank_you'
-  get  'simple_pages/users'
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
